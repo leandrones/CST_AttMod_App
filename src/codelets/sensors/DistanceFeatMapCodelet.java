48eff817587de3123ca6fabafd938adddf71b0ee
Leandro Nascimento Gonçalves de Araujo <leandronasc3@gmail.com>
2018-08-06 20:23:45 +0200

    lgpl 3.0

--- src/codelets/sensors/DistanceFeatMapCodelet.java
+++ src/codelets/sensors/DistanceFeatMapCodelet.java
@@ -1,19 +1,15 @@
 /*
- * Copyright (C) 2018 leandro
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ * /*******************************************************************************
+ *  * Copyright (c) 2012  DCA-FEEC-UNICAMP
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the GNU Lesser Public License v3
+ *  * which accompanies this distribution, and is available at
+ *  * http://www.gnu.org/licenses/lgpl.html
+ *  * 
+ *  * Contributors:
+ *  *     K. Raizer, A. L. O. Paraense, R. R. Gudwin - initial API and implementation
+ *  ******************************************************************************/
+ 
 package codelets.sensors;
 
 import br.unicamp.cst.core.entities.MemoryObject;
